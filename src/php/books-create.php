838e5ae35c670e2492ca18f114ac87ed3134706e
DeezjaVu <p.ginneberge@gmail.com>
2019-11-17 18:30:20 +0100

    Added CORS headers for Vue front-end

--- src/php/books-create.php
+++ src/php/books-create.php
@@ -3,6 +3,7 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Origin: *");
 
 define("BOOKS_URL", "../data/books.json");
 
@@ -13,6 +14,7 @@ $json = file_get_contents(BOOKS_URL);
 $data = json_decode(file_get_contents("php://input"));
 
 // Create unique id for new data entry
+// $data['id'] = new stdClass();
 $data->id = uniqid();
 
 // Decode existing books and add new book at the beginning of the list.
--- src/php/books-read.php
+++ src/php/books-read.php
@@ -2,6 +2,7 @@
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header('Access-Control-Allow-Origin: *');
 
 define("BOOKS_URL", "../data/books.json");
 
